2f0d6a27f5744aa255de882e952a57694ebe789c
Yev <yevgeniybarkalov@yahoo.com>
2018-03-14 03:12:12 +0000

    hello

---
+++ script.py
@@ -0,0 +1,9 @@
+import spacy
+from nltk import Tree
+
+en_nlp = spacy.load("en")
+
+doc = en_nlp(u'The quick brown fox jumps over the lazy dog and the magical unicorn flew over the rainbow.')
+
+for token in doc:
+    #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,token.shape_, token.is_alpha, token.is_stop)
